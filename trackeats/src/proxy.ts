7e9e097079eb9a1487c7d92d3c5063638efc3114
Akshat Pratap Singh <ersinghakshat01@gmail.com>
2026-03-10 00:51:45 +0530

    tried to fix the code

--- trackeats/src/proxy.ts
+++ trackeats/src/proxy.ts
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-import { auth } from "./auth";
 import { getToken } from "next-auth/jwt";
 
 export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
-  const publicRoutes = ["/login", "/register", "/api/auth", "/unauthorized"];
+  const publicRoutes = ["/login", "/register", "/api/auth", "/unauthorised"];
   if (publicRoutes.some((path) => pathname.startsWith(path))) {
     return NextResponse.next();
   }
@@ -18,23 +17,16 @@ export async function proxy(req: NextRequest) {
     loginurl.searchParams.set("callbackUrl", req.url);
     return NextResponse.redirect(loginurl);
   }
-  const session = await auth();
 
-  if (!session) {
-    const loginUrl = new URL("/login", req.url);
-    loginUrl.searchParams.set("callbackUrl", req.url);
-    return NextResponse.redirect(loginUrl);
-  }
-
-  const role = session.user?.role;
+  const role = token.role;
   if (pathname.startsWith("/user") && role !== "user") {
-    return NextResponse.redirect(new URL("/unauthorized", req.url));
+    return NextResponse.redirect(new URL("/unauthorised", req.url));
   }
   if (pathname.startsWith("/delivery") && role !== "deliveryBoy") {
-    return NextResponse.redirect(new URL("/unauthorized", req.url));
+    return NextResponse.redirect(new URL("/unauthorised", req.url));
   }
   if (pathname.startsWith("/admin") && role !== "admin") {
-    return NextResponse.redirect(new URL("/unauthorized", req.url));
+    return NextResponse.redirect(new URL("/unauthorised", req.url));
   }
   return NextResponse.next();
 }
